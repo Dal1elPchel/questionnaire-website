282e36dc8547266f4690dd3ecb787ebf33ad5d03
ÐDal1elPchel <fffpchelintsev@yandex.ru>
2025-04-07 20:53:32 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+const start_button = document.querySelector('#start_button');
+
+start_button.addEventListener('click', () => {
+    const question_randomer = document.querySelector('#rand_question').value;
+    const variants_randomer = document.querySelector('#rand_answer').value;
+    const timer = document.querySelector('#timer').value;
+    window.location.href = "victory_form.html";
+})
